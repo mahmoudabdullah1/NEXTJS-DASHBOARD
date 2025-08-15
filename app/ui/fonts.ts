c320f25df4bd43e6206bd2a65e08d44f9db0dfed
mahmoudabdullah1 <mhmudabdullh1@gmail.com>
2025-08-15 17:36:00 +0300

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Roboto } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+
+export const lusitana = Roboto({ subsets: ["latin"], weight: ["400","700"] });
